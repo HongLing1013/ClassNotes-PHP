f4af53cf17e9af6a1786f3d9d35b54e9cfa1b32c
Hollie <tsubasababy1013@gmail.com>
2022-05-14 11:40:58 +0800

    新增使用POST做BMI頁面

---
+++ form/bmi_post/computing.php
@@ -0,0 +1,28 @@
+<!-- 運算頁 -->
+<?php 
+
+$height=$_POST['height'];
+$weight=$_POST['weight'];
+
+$bmi=round($weight/(($height/100)*($height/100)),1);
+
+// 請根據BMI值,在運算頁上計算出中文的BMI結果或建議,show在結果頁上
+$result='';
+
+if($bmi>=35){
+    $result= "重度肥胖";
+}else if($bmi>=30&&$bmi<35){
+    $result= "中度肥胖";
+}else if($bmi>=27&&$bmi<30){
+    $result= "輕度肥胖";
+}else if($bmi>=24&&$bmi<27){
+    $result= "過重";
+}else if($bmi>=18.5&&$bmi<24){
+    $result= "健康";
+}else if($bmi<18.5){
+    $result= "過輕";
+}
+
+header("location:result.php?bmi=$bmi&result=$result");
+
+?>
