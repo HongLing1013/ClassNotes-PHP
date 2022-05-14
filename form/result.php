1c60475c0e70e952d77b35a3059d427a5ccc3c40
Hollie <tsubasababy1013@gmail.com>
2022-05-14 10:36:51 +0800

    在BMI結果頁判斷BMI範圍文字

--- form/result.php
+++ form/result.php
@@ -13,9 +13,30 @@
     <?php
         $bmi=$_GET['bmi'];
         echo "你的BMI為：".$bmi;
+        echo "<br>";
     ?>
-    <br>
-    <a href="bmi.html"><button>回到BMI計算</button></a>
+    <!-- 請根據BMI值,在結果頁上show出中文的BMI結果或建議 -->
+    <?php
+    if($bmi>=35){
+        echo "重度肥胖";
+    }else if($bmi>=30&&$bmi<35){
+        echo "中度肥胖";
+    }else if($bmi>=27&&$bmi<30){
+        echo "輕度肥胖";
+    }else if($bmi>=24&&$bmi<27){
+        echo "過重";
+    }else if($bmi>=18.5&&$bmi<24){
+        echo "健康";
+    }else if($bmi<18.5){
+        echo "過輕";
+    }
+    ?>
+
+
+
+    <div style="text-align:center;">
+        <a href="bmi.html"><button>回到BMI計算</button></a>
+    </div>
     </h1>
 </body>
 </html>
