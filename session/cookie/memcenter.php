4bbb0fc8f2f079d86203ce20395b368463de6471
Hollie <tsubasababy1013@gmail.com>
2022-05-16 10:59:15 +0800

    使用cookie作登入登出系統

--- session/cookie/memcenter.php
+++ session/cookie/memcenter.php
@@ -8,5 +8,7 @@
 </head>
 <body>
     <h1>歡迎<?=$_COOKIE['login'];?></h1>
+    <li><a href="index.php">回首頁</a></li>
+    <li><a href="logout.php">登出</a></li>
 </body>
 </html>
---
+++ session/session/chklogin.php
@@ -0,0 +1,23 @@
+<?php
+// 預設可以比對的帳號密碼
+$default_user='hollie';
+$default_pw='1234';
+
+// 接收上一個頁面的資料
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+
+// 給他一個狀態 暫時只給空字串
+$error='';
+
+// 判斷式
+if($acc!=$default_user || $pw!=$default_pw){
+    $error= "帳號密碼錯誤";
+    // 帳號密碼錯誤的話 將頁面帶回前端頁面
+    header("location:login.php?error=$error");
+}else{
+    setcookie('login',$acc,time()+360);
+    header("location:memcenter.php?user=$acc");
+}
+
+?>
